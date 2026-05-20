d9a48a31b97d6c73ae948b0369c1c20704bfa5f6
Hinoto-LH <masseron.a.pro@gmail.com>
2026-05-20 09:33:23 +0200

    Add teacher3 example to task_1 Teacher interface

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -6,3 +6,19 @@ interface Teacher {
 	location: string;
 	[key: string]: any;
 }
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
+
+interface Directors {
+	firstName: 'string';
+	lastName: 'string';
+	fullTimeEmployee
+}
